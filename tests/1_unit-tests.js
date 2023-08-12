eaba2029caadd09352dd1fc55f917f7670730e14
ritsudo ritsudo <rikudotk@gmail.com>
2023-08-13 02:56:58 +0300

    Added some tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,43 +2,33 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const puzzlesAndSolutions = require('../controllers/puzzle-strings.js');
+let solver = new Solver();
 
 suite('Unit Tests', () => {
-    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
+    test("Logic handles a valid puzzle string of 81 characters", function() {
+		let testResult = solver.solve(puzzlesAndSolutions.puzzlesAndSolutions[0][0])
+		assert.equal(testResult.solution, puzzlesAndSolutions.puzzlesAndSolutions[0][1]);
     });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
+	test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", function() {
+		let testResult = solver.solve('1.5..2.84..63.12.7.2..5.....9..1.,..8.2.3674.3.7.2..9.47...8..1..16....926914.37.')
+		assert.equal(testResult.error, 'Invalid characters in puzzle');
     });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
+	test("Logic handles a puzzle string that is not 81 characters in length", function() {
+		let testResult = solver.solve('..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.')
+		assert.equal(testResult.error, 'Expected puzzle to be 81 characters long');
     });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
+//    
+//    
+//    
+//    Logic handles a valid row placement
+//    Logic handles an invalid row placement
+//    Logic handles a valid column placement
+//    Logic handles an invalid column placement
+//    Logic handles a valid region (3x3 grid) placement
+//    Logic handles an invalid region (3x3 grid) placement
+//    Valid puzzle strings pass the solver
+//    Invalid puzzle strings fail the solver
+//    Solver returns the expected solution for an incomplete puzzle
+
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,47 +6,34 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-	    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-		    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
-		    test("GET request to /api/stock-prices/", function() {
-      assert.isNotNull(1, '1 is not null');
-    });
+	test("Solve a puzzle with valid puzzle string: POST request to /api/solve", function(done) {
+		chai
+			.request(server)
+			.post("/api/solve")
+			.send({
+				puzzle: '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'
+			})
+			.end(function(err,res){
+				assert.equal(res.status, 200)
+				assert.equal(res.body.solution, "769235418851496372432178956174569283395842761628713549283657194516924837947381625")
+				done();
+			})
+	})
+	
+//  
+//  Solve a puzzle with missing puzzle string: POST request to /api/solve
+//  Solve a puzzle with invalid characters: POST request to /api/solve
+//  Solve a puzzle with incorrect length: POST request to /api/solve
+//  Solve a puzzle that cannot be solved: POST request to /api/solve
+//  Check a puzzle placement with all fields: POST request to /api/check
+//  Check a puzzle placement with single placement conflict: POST request to /api/check
+//  Check a puzzle placement with multiple placement conflicts: POST request to /api/check
+//  Check a puzzle placement with all placement conflicts: POST request to /api/check
+//  Check a puzzle placement with missing required fields: POST request to /api/check
+//  Check a puzzle placement with invalid characters: POST request to /api/check
+//  Check a puzzle placement with incorrect length: POST request to /api/check
+//  Check a puzzle placement with invalid placement coordinate: POST request to /api/check
+//  Check a puzzle placement with invalid placement value: POST request to /api/check
+
 });
 
